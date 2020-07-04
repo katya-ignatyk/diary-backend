60f76abee12555ceca6ea14729b3bf9953eda96d
Katsiaryna Ihnatsiuk <Katsiaryna.Ihnatsiuk@itechart-group.com>
2020-07-04 09:37:27 +0300

    change package.json file, add spaces between type and variable declaration

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import {port} from './config';
 
 const app = express();
 
-app.get('/', ( req:express.Request, res:express.Response) => res.send("hello"));
+app.get('/', ( req : express.Request, res : express.Response) => res.send("hello"));
 
 app.listen(port, () => {
     console.log( `server started at ${port}` );
